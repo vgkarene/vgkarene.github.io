d0ab9caee35ed82af99aa2f57c77445a0be43540
Karène VENG <karene.veng@gmail.com>
2023-02-10 11:22:08 +0100

    Premier envoi

---
+++ script.js
@@ -0,0 +1,9 @@
+// Mettre un style Recommander
+function mettreStyleRecommander() {
+  document.getElementById("Photos").classList.add('recommander');
+}
+
+// Enlever le style Recommander
+function enleveStyleRecommander() {
+  document.getElementById("Photos").classList.remove('recommander');
+}
